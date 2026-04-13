5a3fe7d8acad2fdfbf81be496c7de8e47dd41079
bullets012 <lancegenesismercado0111@gmail.com>
2026-04-13 15:52:36 +0800

    first commit

---
+++ rest/v1/developers/controllers/settings/roles/create.php
@@ -0,0 +1,12 @@
+<?php 
+
+// check database connection 
+$conn = null;
+$conn = checkDbConnection($conn);
+
+
+// make use of classes
+
+$name = $data['name'];
+
+echo "Hello";
---
+++ src/pages/developer/settings/roles/Roles.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../../Layout";
+
+const Roles = () => {
+  return (
+    <>
+      <Layout menu="settings" submenu="roles"></Layout>
+      <div>Roles Content</div>
+    </>
+  );
+};
+
+export default Roles;
